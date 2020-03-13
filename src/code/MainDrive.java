8417960acc19681ad9a78fe507dacbe3cf5a9e9e
cho881020 <kj_cho@nepp.kr>
2020-03-13 11:36:43 +0900

    프로그램 메뉴 기본 구조

--- src/code/MainDrive.java
+++ src/code/MainDrive.java
@@ -1,9 +1,31 @@
 package code;
 
+import java.util.Scanner;
+
 public class MainDrive {
 
 	public static void main(String[] args) {
+		printMenu();
+	}
+	
+	static void printMenu() {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("*****전화번호부*****");
+		System.out.println("1. 전화번호 등록");
+		System.out.println("2. 전화번호 목록 조회");
+		System.out.println("0. 프로그램 종료");
+		System.out.println("******************");
+		System.out.print("메뉴 선택 : ");
+		int userInput = scan.nextInt();
 		
 	}
 	
+	
 }
+
+
+
+
+
